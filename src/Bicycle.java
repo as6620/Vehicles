4b0fa19933f49eac2cbff0d2fbbeb0076730e52d
as6620 <as6620@bs.amalnet.k12.il>
2024-10-08 16:01:46 +0300

    Bicycle

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,2 +1,23 @@
 public class Bicycle extends Light{
+    private boolean isBasket;
+
+    public Bicycle(int numCar, int ageCar, int quantityWheels, double pollutantEmissionsPerMinute, String steeringShape, boolean isEngine, boolean isBasket) {
+        super(numCar, ageCar, quantityWheels, pollutantEmissionsPerMinute, steeringShape, isEngine);
+        this.isBasket = isBasket;
+    }
+
+    public boolean isBasket() {
+        return isBasket;
+    }
+
+    public void setBasket(boolean basket) {
+        isBasket = basket;
+    }
+
+    @Override
+    public String toString() {
+        return "Bicycle{" +
+                "isBasket=" + isBasket +
+                '}';
+    }
 }
