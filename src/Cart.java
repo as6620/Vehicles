6f37969af2b89d8d4030e34712e837b4574e0306
as6620 <as6620@bs.amalnet.k12.il>
2024-10-08 16:02:23 +0300

    Cart

--- src/Cart.java
+++ src/Cart.java
@@ -1,2 +1,23 @@
 public class Cart extends Light{
+    private double chargeTime;
+
+    public Cart(int numCar, int ageCar, int quantityWheels, double pollutantEmissionsPerMinute, String steeringShape, boolean isEngine, double chargeTime) {
+        super(numCar, ageCar, quantityWheels, pollutantEmissionsPerMinute, steeringShape, isEngine);
+        this.chargeTime = chargeTime;
+    }
+
+    public double getChargeTime() {
+        return chargeTime;
+    }
+
+    public void setChargeTime(double chargeTime) {
+        this.chargeTime = chargeTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "chargeTime=" + chargeTime +
+                '}';
+    }
 }
